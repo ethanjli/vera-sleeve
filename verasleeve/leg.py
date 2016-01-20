6e748776cde221266be30b81c357ea98221b4618
Ethan Li <lietk12@gmail.com>
2016-01-20 14:38:57 -0800

    Made flexiforce sensor monitor test
    Currently broken
    Updated leg module to monitor flexiforce sensor

--- verasleeve/leg.py
+++ verasleeve/leg.py
@@ -10,7 +10,8 @@ from serial.serialutil import SerialException
 from verasleeve import actors
 
 # Device parameters
-FLUID_SENSOR_PORT = 'A0'
+FLUID_SENSOR_PORT = 'A5'
+SURFACE_SENSOR_PORT = 'A0'
 
 class Leg(object):
     """Models the Arduino controller of the leg model test fixture."""
@@ -21,9 +22,14 @@ class Leg(object):
             self._board = nanpy.ArduinoApi(connection=connection)
         except SerialException:
             raise RuntimeError("Could not connect to the Arduino!") from None
+        self._board.pinMode(FLUID_SENSOR_PORT, self._board.INPUT)
+        self._board.pinMode(SURFACE_SENSOR_PORT, self._board.INPUT)
 
     def get_fluid_pressure_sensor(self):
         return self._board.analogRead(FLUID_SENSOR_PORT)
+
+    def get_surface_pressure_sensor(self):
+        return self._board.analogRead(SURFACE_SENSOR_PORT)
 
 class LegMonitor(actors.Broadcaster, actors.Producer):
     """An actor to interface between a Leg instance and other actors.
@@ -53,6 +59,10 @@ class LegMonitor(actors.Broadcaster, actors.Producer):
                         'time': self.__time_since_produce_start(),
                         'fluid pressure': self.__leg.get_fluid_pressure_sensor()},
                        'fluid pressure')
+        self.broadcast({'data': 'surface pressure',
+                        'time': self.__time_since_produce_start(),
+                        'surface pressure': self.__leg.get_surface_pressure_sensor()},
+                       'surface pressure')
 
     def __time_since_produce_start(self):
         return time.time() - self.__produce_start_time
