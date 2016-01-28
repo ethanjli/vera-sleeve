4a122dceb3ccbb0aeeeaccc7b3eb9710fdf737a5
Ethan Li <lietk12@gmail.com>
2016-01-28 14:00:21 -0800

    LabelUpdaters can now specify a custom label name

--- verasleeve/gui.py
+++ verasleeve/gui.py
@@ -11,11 +11,13 @@ class LabelUpdater(pykka.ThreadingActor):
             The data entry should specify the key of the entry holding the value of the data
             sample. This key will be printed with the data value.
     """
-    def __init__(self, label):
+    def __init__(self, label, label_name_override=None):
         super().__init__()
         self.label = label
+        self.label_name = label_name_override
 
     def on_receive(self, message):
         """Slot that updates the text label with the next sample."""
-        self.label.setText("{}: {}".format(message['type'], message['data']))
+        label_name = message['type'] if self.label_name is None else self.label_name
+        self.label.setText("{}: {:.1f}".format(label_name, message['data']))
 
