193d9835fd3a814b4f59f6cb724ad89b59874af9
Ethan Li <lietk12@gmail.com>
2016-01-28 14:01:30 -0800

    Added unit conversion for the fluid sensor

--- verasleeve/leg.py
+++ verasleeve/leg.py
@@ -5,6 +5,7 @@ import time
 # Dependency imports
 import nanpy
 from serial.serialutil import SerialException
+import pykka
 
 # Package imports
 from verasleeve import actors
@@ -14,6 +15,9 @@ from verasleeve import actors
 FLUID_SENSOR_PIN = 0
 SURFACE_SENSOR_PINS = [1]
 SURFACE_SENSOR_IDS = range(len(SURFACE_SENSOR_PINS))
+
+# Default unit conversion functions for sensors
+FLUID_PRESSURE_RAW_TO_MMHG = lambda raw: (raw - 100) / 0.535
 
 class Leg(object):
     """Models the Arduino controller of the leg model test fixture."""
@@ -68,3 +72,40 @@ class LegMonitor(actors.Broadcaster, actors.Producer):
 
     def __time_since_produce_start(self):
         return time.time() - self.__produce_start_time
+
+class LegUnitConverter(actors.Broadcaster, pykka.ThreadingActor):
+    """Converts raw sensor value data from a LegMonitor into physical units.
+    Passes through any data messages it doesn't recognize as amenable to unit conversion.
+
+    Public Messages:
+        Data (received):
+            Data messages should have a time entry holding the time since the producer started
+            emitting messages at which the data sample was recorded. The type entry should specify
+            the type of data message, while the data entry holds the raw sensor value of the
+            data sample.
+            fluid pressure: the raw reading from the fluid pressure sensor.
+            surface pressure n: the raw reading from the surface pressure sensor with id n.
+        Data (broadcasted):
+            Data messages have a time entry holding the time since the producer started
+            emitting messages at which the data sample was recorded. Each data message is
+            broadcast on the broadcast class corresponding to the type of data message,
+            e.g. 'fluid pressure'.
+            The type entry specifies the type of data message, while the data entry holds
+            the value of the data sample.
+            fluid pressure: the reading from the fluid pressure sensor.
+            surface pressure n: the reading from the surface pressure sensor with id n.
+    """
+    def __init__(self, raw_to_fluid_pressure=FLUID_PRESSURE_RAW_TO_MMHG):
+        super().__init__()
+        self.raw_to_fluid_pressure = raw_to_fluid_pressure
+
+    def on_receive(self, message):
+        self.__on_data(message)
+
+    def __on_data(self, message):
+        """Processes data messages."""
+        new_message = dict(message)
+        if message['type'] == 'fluid pressure':
+            new_message['data'] = self.raw_to_fluid_pressure(message['data'])
+        self.broadcast(new_message, new_message['type'])
+
