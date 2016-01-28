ba1c44a3cd2e4efbd1b5b109fd6122881192c626
Ethan Li <lietk12@gmail.com>
2016-01-28 14:00:55 -0800

    Filterer can now be centered or right-aligned.
    This controls the time associated with the filtered sample output

--- verasleeve/signal.py
+++ verasleeve/signal.py
@@ -16,15 +16,18 @@ def get_interpolator(x_y, left_limit, right_limit):
                                             left_limit, right_limit)[0]
 
 @coroutines.initialized_coroutine
-def moving_filter(max_samples=None, filterer=np.median):
+def moving_filter(max_samples=None, filterer=np.median, mode="centered"):
     """A coroutine to filter a signal using its max_samples most recent samples.
     To initialize, assign to a variable and start using it - no need to call the next
     function on it or send in an initial None value.
 
     Arguments:
         max_samples: the window size over which to compute the filtered sample.
-        To make the filter exactly symmetric, set this to be an odd number. If this
-        is None, the moving filter processes all samples.
+        To make the filter exactly symmetric in centered mode, set this to be an odd number. If this
+        is None, the moving filter processes all samples and is effectively in right mode.
+        mode: either "centered" or "right". If centered, the output sample number will correspond
+        to the sample number for the (max_samples / 2)th most recent sample. If right, the output
+        sample number will correspond to the sample number for the most recent sample.
 
     Sending:
         Send a two-tuple of the sample number (or sample time) and value into moving_filter to add
@@ -37,8 +40,7 @@ def moving_filter(max_samples=None, filterer=np.median):
         None: if the filter has not yet collected max_samples samples.
         Otherwise, a two-tuple of the sample number (or sample time) for the filtered sample and
         the filtered result (as computed by filterer) of the max_samples most recent samples.
-        The sample number will correspond to the sample number for the (max_samples / 2)th most
-        recent sample.
+        The sample number will be calculated depending on the mode argument.
     """
     signal_buffer = deque(maxlen=max_samples)
     sample_numbers = deque(maxlen=(max_samples // 2 + 1
@@ -58,9 +60,9 @@ def moving_filter(max_samples=None, filterer=np.median):
             sample_numbers.append(value[0])
             signal_buffer.append(value[1])
             num_samples = len(signal_buffer)
-            if max_samples is None:
+            if max_samples is None or mode == "right":
                 filtered = (sample_numbers[-1], filterer(signal_buffer))
-            elif num_samples == max_samples:
+            elif num_samples == max_samples and mode == "centered":
                 filtered = (sample_numbers[0], filterer(signal_buffer))
             else:
                 filtered = None
@@ -82,9 +84,9 @@ class Filterer(actors.Broadcaster, pykka.ThreadingActor):
             The data entry specifies the value of the data sample.
             The data sample will be a filtered value.
     """
-    def __init__(self, filter_width=None, filterer=np.median):
+    def __init__(self, filter_width=None, filterer=np.median, mode="centered"):
         super().__init__()
-        self.filterer = moving_filter(filter_width, filterer)
+        self.filterer = moving_filter(filter_width, filterer, mode)
 
     def on_receive(self, message):
         if 'command' in message:
