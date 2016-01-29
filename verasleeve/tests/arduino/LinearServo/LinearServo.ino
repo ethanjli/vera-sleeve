d4a152ae8fb937029adbeb194d06ba4be6e110f6
Ethan Li <lietk12@gmail.com>
2016-01-28 18:44:32 -0800

    Added additional servo pin that goes in reverse
    This is because some servo motors seem to go in reverse.

--- verasleeve/tests/arduino/LinearServo/LinearServo.ino
+++ verasleeve/tests/arduino/LinearServo/LinearServo.ino
@@ -6,10 +6,13 @@
  #include <Servo.h>
 
  Servo testServo;
+ Servo testServo2;
  int servoPosition = 0;
+ int servo2Position = 0;
 
 const int kSerialRate = 9600; // Rate of serial data
 const int kServoPin = 12; // Servo output pin
+const int kServo2Pin = 11; // Servo output pin
 const int kAdjustmentInterval = 50; // Delay between ADC readings to settle between readings
 const int kServoStep = 1; // Servo adjustment distance
 const int kServoMin = 55;
@@ -20,16 +23,23 @@ void setup() {
   Serial.begin(kSerialRate);
   // initialize servo
   testServo.attach(kServoPin);
+  testServo2.attach(kServo2Pin);
 }
 
 void loop() {
+  servo2Position = kServoMax;
   for (servoPosition = kServoMin; servoPosition <= kServoMax; servoPosition += kServoStep) {
     testServo.write(servoPosition);
+    testServo2.write(servo2Position);
+    servo2Position -= kServoStep;
     Serial.println(servoPosition);
   delay(kAdjustmentInterval);
   }
+  servo2Position = kServoMin;
   for (servoPosition = kServoMax; servoPosition >= kServoMin; servoPosition -= kServoStep) {
     testServo.write(servoPosition);
+    testServo2.write(servo2Position);
+    servo2Position += kServoStep;
     Serial.println(servoPosition);
   delay(kAdjustmentInterval);
   }
