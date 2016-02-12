cfe02af82a9fe661d8dabcdf45db140c910498a6
Ethan Li <lietk12@gmail.com>
2016-02-11 20:25:55 -0800

    Linear Servo test now drives square waves
    Testing showed that triangle waves stalled at lower force

--- verasleeve/tests/arduino/LinearServo/LinearServo.ino
+++ verasleeve/tests/arduino/LinearServo/LinearServo.ino
@@ -3,20 +3,21 @@
  * Use with the Arduino IDE's Serial Monitor or Serial Plotter.
  */
 
- #include <Servo.h>
+#include <Servo.h>
 
- Servo testServo;
- Servo testServo2;
- int servoPosition = 0;
- int servo2Position = 0;
+Servo testServo;
+Servo testServo2;
+Servo testServo3;
 
 const int kSerialRate = 9600; // Rate of serial data
-const int kServoPin = 12; // Servo output pin
-const int kServo2Pin = 11; // Servo output pin
-const int kAdjustmentInterval = 50; // Delay between ADC readings to settle between readings
-const int kServoStep = 1; // Servo adjustment distance
+const int kServoPin = 12; // Spektrum servo output pin
+const int kServo2Pin = 11; // Spektrum reversed servo output pin
+const int kServo3Pin = 10; // Firgelli servo output pin
+const int kAdjustmentInterval = 3000; // Delay between ADC readings to settle between reading
 const int kServoMin = 55;
 const int kServoMax = 130;
+const int kServo3Min = 35;
+const int kServo3Max = 170;
 
 void setup() {
   // initialize serial communications
@@ -24,23 +25,16 @@ void setup() {
   // initialize servo
   testServo.attach(kServoPin);
   testServo2.attach(kServo2Pin);
+  testServo3.attach(kServo3Pin);
 }
 
 void loop() {
-  servo2Position = kServoMax;
-  for (servoPosition = kServoMin; servoPosition <= kServoMax; servoPosition += kServoStep) {
-    testServo.write(servoPosition);
-    testServo2.write(servo2Position);
-    servo2Position -= kServoStep;
-    Serial.println(servoPosition);
+  testServo.write(kServoMin);
+  testServo2.write(kServoMax);
+  testServo3.write(kServo3Max);
   delay(kAdjustmentInterval);
-  }
-  servo2Position = kServoMin;
-  for (servoPosition = kServoMax; servoPosition >= kServoMin; servoPosition -= kServoStep) {
-    testServo.write(servoPosition);
-    testServo2.write(servo2Position);
-    servo2Position += kServoStep;
-    Serial.println(servoPosition);
+  testServo.write(kServoMax);
+  testServo2.write(kServoMin);
+  testServo3.write(kServo3Min);
   delay(kAdjustmentInterval);
-  }
 }
