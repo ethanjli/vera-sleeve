7f22d833e3fe07a41e075d70880d8e8e16dc7d3a
Ethan Li <lietk12@gmail.com>
2016-02-18 02:12:11 -0800

    Added basic sleeve controller classes.

--- verasleeve/sleeve.py
+++ verasleeve/sleeve.py
@@ -43,3 +43,49 @@ class SleeveServos(object):
         watchdog = Watchdog(self.__connection)
         watchdog.enable(0)
         time.sleep(0.2)
+
+class SleeveController(actors.Producer):
+    """An abstract actor to control the contractions of a leg sleeve."""
+    def __init__(self, sleeve_servos=None):
+        super().__init__()
+        if sleeve_servos is None:
+            sleeve_servos = SleeveServos()
+        self.__sleeve_servos = sleeve_servos
+        self.__produce_start_time = None
+
+    def _on_start_producing(self):
+        self.__produce_start_time = time.time()
+    def _on_stop_producing(self):
+        print("stop producing")
+        self.__produce_start_time = None
+    def _on_produce(self):
+        for servo_id in BAND_SERVO_IDS:
+            self.__sleeve_servos.set_servo_position(servo_id, self._get_position(servo_id))
+
+    def on_stop(self):
+        self.__sleeve_servos.quit()
+
+    def _time_since_produce_start(self):
+        return time.time() - self.__produce_start_time
+    def _get_position(self, servo_id):
+        """Abstract method returning a servo position for the specified servo.
+        Implement this method to define sleeve contraction behavior."""
+        return 0
+
+class AdditiveSleeveController(SleeveController):
+    """Contracts sleeve bands in an additive sequential square-wave manner."""
+    def __init__(self, sleeve=None, period=2 * (1 + NUM_BANDS), duty=1 / (1 + NUM_BANDS),
+                 delay_per_band=2, min_pos=130, max_pos=50):
+        super().__init__(sleeve)
+        self.period = period
+        self.duty = duty
+        self.delay_per_band = delay_per_band
+        self.min_pos = min_pos
+        self.max_pos = max_pos
+
+    def __time_since_cycle_start(self):
+        return self._time_since_produce_start() % self.period
+    def _get_position(self, servo_id):
+        adjusted_time = self.__time_since_cycle_start() - self.delay_per_band * servo_id
+        whether_contract = (adjusted_time < self.period and adjusted_time > self.duty * self.period)
+        return self.min_pos + (self.max_pos - self.min_pos) * int(whether_contract)
---
+++ verasleeve/tests/additive_sleeve.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Tests the fluid sensor."""
+# Python imports
+import logging
+import time
+
+# Dependency imports
+import pykka
+
+# Package imports
+from .. import sleeve
+
+logging.basicConfig(level=logging.INFO)
+
+def control_band():
+    """Continuously prints the pin value on the fluid pressure sensor."""
+    logger = logging.getLogger(__name__)
+
+    try:
+        sleeve_controller = sleeve.AdditiveSleeveController.start()
+    except RuntimeError:
+        pykka.ActorRegistry.stop_all() # stop actors in LIFO order
+        raise
+    logger.info("Controlling sleeve...")
+    sleeve_controller.tell({'command': 'start producing', 'interval': 0.1})
+    time.sleep(30)
+    logger.info("Quitting...")
+    pykka.ActorRegistry.stop_all()
+
+if __name__ == "__main__":
+    control_band()
