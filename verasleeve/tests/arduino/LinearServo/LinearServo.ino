7ee4697a1bd8283b55600c882a9d33c73790b210
Ethan Li <lietk12@gmail.com>
2016-02-29 14:20:40 -0800

    Simplified LinearServo test and added blink indicator.

--- verasleeve/tests/arduino/LinearServo/LinearServo.ino
+++ verasleeve/tests/arduino/LinearServo/LinearServo.ino
@@ -6,35 +6,37 @@
 #include <Servo.h>
 
 Servo testServo;
-Servo testServo2;
-Servo testServo3;
 
-const int kSerialRate = 9600; // Rate of serial data
-const int kServoPin = 12; // Spektrum servo output pin
-const int kServo2Pin = 11; // Spektrum reversed servo output pin
-const int kServo3Pin = 10; // Firgelli servo output pin
+const int kServoPin = 11; // Firgelli servo output pin
 const int kAdjustmentInterval = 3000; // Delay between ADC readings to settle between reading
-const int kServoMin = 55;
+const int kServoMin = 50;
 const int kServoMax = 130;
-const int kServo3Min = 35;
-const int kServo3Max = 170;
+const int blinkPin = 13;
 
 void setup() {
-  // initialize serial communications
-  Serial.begin(kSerialRate);
   // initialize servo
   testServo.attach(kServoPin);
-  testServo2.attach(kServo2Pin);
-  testServo3.attach(kServo3Pin);
+  pinMode(blinkPin, OUTPUT);
+
 }
 
 void loop() {
-  testServo.write(kServoMin);
-  testServo2.write(kServoMax);
-  testServo3.write(kServo3Max);
-  delay(kAdjustmentInterval);
   testServo.write(kServoMax);
-  testServo2.write(kServoMin);
-  testServo3.write(kServo3Min);
-  delay(kAdjustmentInterval);
+  digitalWrite(blinkPin, HIGH);
+  delay(kAdjustmentInterval / 4);
+  digitalWrite(blinkPin, LOW);
+  delay(kAdjustmentInterval / 4);
+  digitalWrite(blinkPin, HIGH);
+  delay(kAdjustmentInterval / 4);
+  digitalWrite(blinkPin, LOW);
+  delay(kAdjustmentInterval / 4);
+  testServo.write(kServoMin);
+  digitalWrite(blinkPin, HIGH);
+  delay(kAdjustmentInterval / 4);
+  digitalWrite(blinkPin, LOW);
+  delay(kAdjustmentInterval / 4);
+  digitalWrite(blinkPin, HIGH);
+  delay(kAdjustmentInterval / 4);
+  digitalWrite(blinkPin, LOW);
+  delay(kAdjustmentInterval / 4);
 }
